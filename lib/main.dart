c91a38fbf80d67ec2fcefd2b5af1e7fd6463c922
MElvis78 <elvismukuru23@gmail.com>
2023-09-05 05:03:09 +0300

    dev commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  final Future<FirebaseApp> _initialization = Firebase.initializaApp();
 
   // This widget is the root of your application.
   @override
