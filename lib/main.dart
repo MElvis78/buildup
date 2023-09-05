13845442fbb5213e84f7cb3b40ca9e36d83e44e8
MElvis78 <elvismukuru23@gmail.com>
2023-09-05 23:24:53 +0300

    dev commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+// ....
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
 
-void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  final Future<FirebaseApp> _initialization = Firebase.initializaApp();
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(),
-    );
-  }
-}
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot)
+      {
+        if(snapshot.connectionState == ConnectionState.waiting)
+        {
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('BuildupApp is being initialised',
+                style: TextStyle(
+                  color: Colors.cyan,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+               ),),
+              ),
+             ),
+           );
+          }
+          else if (snapshot.hasError)
+          {
+            return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('An error has occured',
+                style: TextStyle(
+                  color: Colors.cyan,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+               ),),
+              ),
+             ),
+           );
+         }
+         return MaterialApp(
+          title: 'Buildup App',
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.black,
+            primarySwatch: Colors.blue,
+            ),
+           home: const Scaffold(), 
+         );
+        }
+       );
+      }
+    }
