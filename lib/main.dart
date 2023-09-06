446965836e8043e3f847a1151f211def56f9b855
MElvis78 <elvismukuru23@gmail.com>
2023-09-06 14:42:19 +0300

    dev commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+//import 'dart:ui_web';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 // ....
 await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-
-  WidgetsFlutterBinding.ensureInitialized();
-  
+ 
   runApp( MyApp());
 }
 
