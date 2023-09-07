18d1efc1b79b2a4d096c671aae0b709cf340a31c
MElvis78 <elvismukuru23@gmail.com>
2023-09-07 05:05:14 +0300

    dev commit

--- lib/SignupPage/signup_screen.dart
+++ lib/SignupPage/signup_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:buildup_application/Services/global_variables.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +16,14 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
  late Animation<double> _animation;
  late AnimationController _animationController;
 
+ final TextEditingController _fullNameController = TextEditingController(text: '');
+ final TextEditingController _emailTextController = TextEditingController(text: '');
+
+ final FocusNode _emailFocusNode = FocusNode();
+ final FocusNode _passFocusNode = FocusNode();
+
  final _signUpFormKey = GlobalKey<FormState>();
+ File? imageFile;
 
  @override
   void dispose(){
@@ -83,10 +92,80 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                                 border: Border.all(width: 1, color: Colors.cyanAccent,),
                                 borderRadius: BorderRadius.circular(20),
                               ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: imageFile == null
+                                ? const Icon(Icons.camera_enhance_sharp, color: Colors.cyan, size: 30,)
+                                : Image.file(imageFile!, fit: BoxFit.fill,),
+                              ),
 
                             ),
                           )
                         ),
+                        const SizedBox(height: 20,),
+                        TextFormField(
+                            textInputAction: TextInputAction.next,
+                            onEditingComplete: () => FocusScope.of(context).requestFocus(_emailFocusNode),
+                            keyboardType: TextInputType.name,
+                            controller: _fullNameController,
+                            validator: (value)
+                            {
+                              if(value!.isEmpty)
+                              {
+                                return "This field is missing.";
+                              }
+                              else
+                              {
+                                return null;
+                              }
+                            },
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              hintText: 'Full name / Company name',
+                              hintStyle: TextStyle(color: Colors.white),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                ),
+                                errorBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          TextFormField(
+                            textInputAction: TextInputAction.next,
+                            onEditingComplete: () => FocusScope.of(context).requestFocus(_passFocusNode),
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _emailTextController,
+                            validator: (value)
+                            {
+                              if(value!.isEmpty || !value.contains('@'))
+                              {
+                                return "Please enter a valid email address";
+                              }
+                              else
+                              {
+                                return null;
+                              }
+                            },
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: TextStyle(color: Colors.white),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                ),
+                                errorBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                            ),
+                          ),
                       ],
                     ),
 
