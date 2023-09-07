0211ac0148eb3b917e90da6b6f79c5d1619bf8d4
MElvis78 <elvismukuru23@gmail.com>
2023-09-07 21:39:12 +0300

    dev commit

--- lib/SignupPage/signup_screen.dart
+++ lib/SignupPage/signup_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:buildup_application/Services/global_variables.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -18,12 +19,21 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
 
  final TextEditingController _fullNameController = TextEditingController(text: '');
  final TextEditingController _emailTextController = TextEditingController(text: '');
+ final TextEditingController _passTextController = TextEditingController(text: '');
+ final TextEditingController _phoneNumberTextController = TextEditingController(text: '');
+ final TextEditingController _locationTextController = TextEditingController(text: '');
+
+
 
  final FocusNode _emailFocusNode = FocusNode();
  final FocusNode _passFocusNode = FocusNode();
+ final FocusNode _phoneNumberFocusNode = FocusNode();
+ final FocusNode _positionCPFocusNode = FocusNode();
 
  final _signUpFormKey = GlobalKey<FormState>();
+ bool _obscureText = true;
  File? imageFile;
+ bool _isLoading = false;
 
  @override
   void dispose(){
@@ -166,6 +176,188 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
                                 ),
                             ),
                           ),
+                          const SizedBox(height: 20,),
+                          TextFormField(
+                            textInputAction: TextInputAction.next,
+                            onEditingComplete: () => FocusScope.of(context).requestFocus(_phoneNumberFocusNode),
+                            keyboardType: TextInputType.visiblePassword,
+                            controller: _passTextController,
+                            obscureText: !_obscureText,
+                            validator: (value)
+                            {
+                              if(value!.isEmpty || value.length <7)
+                              {
+                                return "Please enter a valid password";
+                              }
+                              else
+                              {
+                                return null;
+                              }
+                            },
+                            style: const TextStyle(color: Colors.white),
+                            decoration:  InputDecoration(
+                              suffixIcon: GestureDetector(
+                                onTap: ()
+                                {
+                                  setState(() {
+                                    _obscureText = !_obscureText;
+                                  });
+
+                                },
+                                child: Icon(
+                                  _obscureText
+                                  ?Icons.visibility
+                                  :Icons.visibility_off,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(color: Colors.white),
+                              enabledBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                ),
+                                errorBorder: const UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          TextFormField(
+                            textInputAction: TextInputAction.next,
+                            onEditingComplete: () => FocusScope.of(context).requestFocus(_positionCPFocusNode),
+                            keyboardType: TextInputType.phone,
+                            controller: _phoneNumberTextController,
+                            validator: (value)
+                            {
+                              if(value!.isEmpty)
+                              {
+                                return "This field is missing";
+                              }
+                              else
+                              {
+                                return null;
+                              }
+                            },
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              hintText: 'Phone Number',
+                              hintStyle: TextStyle(color: Colors.white),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                ),
+                                errorBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          TextFormField(
+                            textInputAction: TextInputAction.next,
+                            onEditingComplete: () => FocusScope.of(context).requestFocus(_positionCPFocusNode),
+                            keyboardType: TextInputType.text,
+                            controller: _locationTextController,
+                            validator: (value)
+                            {
+                              if(value!.isEmpty)
+                              {
+                                return "This field is missing";
+                              }
+                              else
+                              {
+                                return null;
+                              }
+                            },
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              hintText: 'Company Address',
+                              hintStyle: TextStyle(color: Colors.white),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                ),
+                                errorBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                            ),
+                          ),
+                          const SizedBox(height: 25,),
+                          _isLoading
+                          ?
+                           Center(
+                            child: Container(
+                              width: 70,
+                              height: 70,
+                              child: CircularProgressIndicator(),
+                            ),
+                           )
+                           :
+                           MaterialButton(
+                            onPressed: (){
+                              //createsubmitFormOnSignUp
+                            },
+                            color: Colors.cyan,
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(13),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 14),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'SignUp',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                            ),
+                            const SizedBox(height: 40,),
+                            Center(
+                              child: RichText(
+                                text: TextSpan(
+                                  children: [
+                                    const TextSpan(
+                                      text: 'Already have an account?',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const TextSpan(
+                                      text: '    '
+                                    ),
+                                    TextSpan(
+                                      recognizer: TapGestureRecognizer()
+                                      ..onTap = () => Navigator.canPop(context)
+                                      ? Navigator.pop(context)
+                                      : null,
+                                      text: 'Login',
+                                      style: const TextStyle(
+                                        color: Colors.cyan,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                       ],
                     ),
 
