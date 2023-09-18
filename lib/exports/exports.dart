55d39929730891af9b4b59df1e52ce5f28d081b4
Mugamba Bruno <brunohectre@gmail.com>
2023-09-18 13:42:23 +0300

    modified project setup

--- lib/exports/exports.dart
+++ lib/exports/exports.dart
@@ -1,8 +1,14 @@
 export 'package:flutter/material.dart';
-
-export '/screens/Auth/login_screen.dart';
-export '/routes/routes.dart';
-export '/screens/Auth/login_screen.dart';
-
+export 'package:flutter/services.dart';
+export 'package:provider/provider.dart';
 export 'package:firebase_core/firebase_core.dart';
+
+// screens
+export '/screens/Auth/login_screen.dart';
+
+// routes
+export '/routes/routes.dart';
+
+// controllers
 export '/firebase_options.dart';
+export '/controllers/AppController.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/services.dart';
-
 import '/exports/exports.dart';
 //import 'dart:ui_web';
 
@@ -14,59 +12,21 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const MaterialApp(
-              home: Scaffold(
-                body: Center(
-                  child: Text(
-                    'BuildupApp is being initialised',
-                    style: TextStyle(
-                      color: Colors.cyan,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Signatra',
-                    ),
-                  ),
-                ),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return const MaterialApp(
-              home: Scaffold(
-                body: Center(
-                  child: Text(
-                    'An error has occured',
-                    style: TextStyle(
-                        color: Colors.cyan,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            );
-          }
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Buildup App',
-            theme: ThemeData(
-              useMaterial3: true,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-            ),
-            routes: GlobalRoutes.routes,
-            initialRoute: GlobalRoutes.loginScreen,
-          );
-        });
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AppController()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Buildup App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        routes: GlobalRoutes.routes,
+        initialRoute: GlobalRoutes.loginScreen,
+      ),
+    ),
+  );
 }
